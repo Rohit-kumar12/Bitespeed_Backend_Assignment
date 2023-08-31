38d1d4e09f4c4a1401f480994dbc32070a78768b
Rohit Kumar Sahoo <99555656+Rohit-kumar12@users.noreply.github.com>
2023-09-01 00:48:46 +0530

    Update ormconfig.ts

--- config/ormconfig.ts
+++ config/ormconfig.ts
@@ -6,7 +6,6 @@ dotenv.config();
 
 const connectDB = new DataSource({
   type: "postgres",
-  // url: "postgres://rohit:FQoH7JNkMf7j6G9Qy2kusHeRTOkTwxSO@dpg-cjoaodj58phs73bm0p10-a:5432/bitespeed_backend",
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USER,
